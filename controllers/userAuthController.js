8fe11befa3a6e3a1ff3b99328d65b2fb1ef941f7
Siddharth <moonmars2au@gmail.com>
2024-09-28 01:05:26 +0530

    This is the 4th commit

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -20,13 +20,17 @@ class UserAuthController {
 
     // All data should exist
     if (!(name && password && email)) {
-      return res.send("Fill the credientials");
+      return res.json({
+        message: "Fill the credientials",
+      });
     }
 
     //   Check if user alredy exist
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.send("email already exist");
+      return res.json({
+        message: "email already exist",
+      });
     }
 
     // encrypt the password
@@ -63,13 +67,17 @@ class UserAuthController {
     const { email, password } = req.body;
 
     if (!(email && password)) {
-      return res.send("Enter email and password");
+      return res.json({
+        message: "Enter email and password",
+      });
     }
 
     const user = await User.findOne({ email }).select("+password");
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.send("Incorrect email or password");
+      return res.status(401).json({
+        message: "Incorrect email or password",
+      });
     }
 
     const token = jwt.sign(
@@ -93,18 +101,18 @@ class UserAuthController {
     const { email } = req.body;
 
     if (!email) {
-      return res.json({
+      return res.status(400).json({
         message: "Enter a valid email",
       });
     }
 
     // Checking if the user exists or not
-    // const user = await User.findOne({ email });
-    // if (!user) {
-    //   return res.json({
-    //     message: "The email does not exist in the database",
-    //   });
-    // }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "The email does not exist in the database",
+      });
+    }
 
     // Generating OTP
     const { otp, expires } = TOTP.generate(`${process.env.OTP_SECRET_KEY}`, {
@@ -115,7 +123,7 @@ class UserAuthController {
     // Sending the otp to the email
 
     // Sending message to client
-    res.json({
+    res.status(200).json({
       message: `Otp has been sent to ${email}`,
       expires,
     });
@@ -124,10 +132,8 @@ class UserAuthController {
   async checkOtp(req, res) {
     const { otp, expiresTime } = req.body;
 
-    console.log(otp);
-
     if (!(otp || expiresTime)) {
-      return res.json({
+      return res.status(400).json({
         message: "Enter the otp",
       });
     }
@@ -136,12 +142,12 @@ class UserAuthController {
     const currentTime = Date.now();
 
     if (currentTime > expiresTime) {
-      return res.json({
+      return res.status(410).json({
         message: "The otp has expired",
       });
     }
 
-    res.json({
+    res.status(200).json({
       message: "You can set new password",
     });
   }
