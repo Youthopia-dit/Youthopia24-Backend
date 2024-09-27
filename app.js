c1aa05e492064de4700d43e8e8b3f8ded371f1f0
Siddharth <moonmars2au@gmail.com>
2024-09-27 21:11:57 +0530

    This is the first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const eventRoutes = require('./routes/events');
-const authRouter = require("./routes/user");
+const express = require("express");
+const eventRoutes = require("./routes/events");
+const authRouter = require("./routes/authRoutes");
 require("dotenv").config();
 
 const connectDB = require("./db");
@@ -10,18 +10,17 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(express.json());  // Parse incoming JSON requests
+app.use(express.json()); // Parse incoming JSON requests
 
 // Routes
-app.use('/api/events', eventRoutes);  // Use the events routes
+app.use("/api/events", eventRoutes); // Use the events routes
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
-  
+  res.send("Hello World!");
+});
+
 app.use("/", authRouter);
 
 // Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
