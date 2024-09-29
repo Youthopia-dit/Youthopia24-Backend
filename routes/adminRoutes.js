c8f5270086c3121ae39901e45683f4ace80d1f0e
Rudraksh Gupta <rudrakshgupta40@gmail.com>
2024-09-29 16:11:38 +0530

    Siddhart Updates Done

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,8 +3,8 @@ const { userSignup, userLogin } = require("../controllers/user");
 
 const router = express.Router();
 
-router.post("/api/v1/signup", userSignup);
+router.post("/api/v2/signup", userSignup);
 
-router.post("/api/v1/login", userLogin);
+router.post("/api/v2/login", userLogin);
 
 module.exports = router;
