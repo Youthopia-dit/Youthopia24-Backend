44a2cb6cdc7b8595cb355fbe511096c734ddbb68
Tanya Singhal <tanyaaggarwal2002@gmail.com>
2024-10-15 09:58:16 +0530

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,30 +1,31 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    }
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//     }
 });
 
 const sendMail = (to, subject, text, html) => {
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        text,
-        html,
-        attachments: [
-            {
-              filename: '   RndmPdf.pdf',
-              path: './assets/RndmPdf.pdf', 
-            },
-        ],
-    };
+    // const mailOptions = {
+    //     from: process.env.EMAIL_USER,
+    //     to,
+    //     subject,
+    //     text,
+    //     html,
+    //     attachments: [
+    //         {
+    //           filename: '   RndmPdf.pdf',
+    //           path: './assets/RndmPdf.pdf', 
+    //         },
+    //     ],
+    // };
 
-    return transporter.sendMail(mailOptions);
+    // return transporter.sendMail(mailOptions);
+    return 0;
 };
 
 module.exports = sendMail;
