d7a814a7fc522270ce887da34fe0ffb8ef7781eb
Siddharth <moonmars2au@gmail.com>
2024-10-18 18:24:37 +0530

    This is the commit for highlight controller

--- Routes/highlightsRoutes.js
+++ Routes/highlightsRoutes.js
@@ -1,21 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Flagships = require('../models/highlightsModel');
-const Event = require('../models/eventModel');
+const highlightController = require('../controllers/highlightController');
 
-router.get('/', async (req, res) => {
-  try {
-    const [{ events }] = await Flagships.find();
-
-    const allEvents = await Event.find({ event_id: { $in: events } });
-
-    res.status(200).json({
-      message: 'All events',
-      data: allEvents,
-    });
-  } catch (err) {
-    res.json(err);
-  }
-});
+router.get('/', highlightController);
 
 module.exports = router;
