b848cac6440b85593cf8cc2d3f567d6166866537
TheDarkLord100 <miracleyt404@gmail.com>
2024-10-21 11:31:18 +0530

    Added getProfile

--- app.js
+++ app.js
@@ -20,23 +20,17 @@ app.use(cors());
 // Middleware
 app.use(express.json()); // Parse incoming JSON requests
 
-// Routes
 app.use("/api/events", eventRoutes); // Use the events routes
 // app.use("/api/sponsor", sponsor);
 // app.use("/api/mailer", mailerRoutes);
 // app.use('/api', eventRegisterRoutes);
 // app.use("/api/docs", docsRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.use("/api/user", authRoutes);
-// app.use("/admin", adminRoutes);
 
-// Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
