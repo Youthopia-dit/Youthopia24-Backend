35664cc6eb8d17f89e3669e24266bf2dd06fb9d4
youthopia-official <youthopia.dituniversity@gmail.com>
2024-10-20 23:12:20 -0700

    production

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const eventRoutes = require("./Routes/events");
 const cors = require("cors");
+const fs = require('fs');
+const https = require('https');
 // const adminRoutes = require("./Routes/adminRoutes");
 // const sponsor = require("./Routes/sponsor");
 const authRoutes = require("./Routes/authRoutes");
@@ -34,9 +36,17 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
+const options = {
+  key: fs.readFileSync('./private.key'),  // Private key
+  cert: fs.readFileSync('./certificate.crt'), // Certificate
+  ca: fs.readFileSync('./ca_bundle.crt')
+};
+
 app.use("/api/user", authRoutes);
 // app.use("/admin", adminRoutes);
 
 // Start Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = 4000;
+const server = https.createServer(options, app);
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
